eaecb39334a14219cc2ca95f7f9ac49ffbd383f6
Douglas MP <douglasmpestana@gmail.com>
2022-06-15 10:13:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// CSS
+import "./App.css";
+
+// React
+import { useCallback, useEffect, useState } from "react";
+
+// Data
+import { wordsList } from "./data/words";
+
+// Components
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+    { id: 1, name: "start" },
+    { id: 2, name: "game" },
+    { id: 3, name: "end" },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [gameStage, setGameStage] = useState(stages[0].name);
+    const [words] = useState(wordsList);
+
+    // starts the secret word game
+    const startGame = () => {
+        setGameStage(stages[1].name);
+    };
+
+    // process the letter input
+    const verifyLetter = () => {
+        setGameStage(stages[2].name);
+    };
+
+    // restarts the game
+    const retry = () => {
+        setGameStage(stages[0].name);
+    };
+
+    return (
+        <div className="App">
+            {gameStage === "start" && <StartScreen startGame={startGame} />}
+            {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+            {gameStage === "end" && <GameOver retry={retry} />}
+        </div>
+    );
 }
 
 export default App;
